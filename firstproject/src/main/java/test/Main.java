80c78084af339672aa09873162a64b711e105768
mehdi <m.elketroussi@gmail.com>
2015-04-13 22:25:43 +0200

    Ajout d'un message

--- firstproject/src/main/java/test/Main.java
+++ firstproject/src/main/java/test/Main.java
@@ -1,9 +1,10 @@
-package test;
+package src.main.java.test;
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("COUCOU");
+		System.out.println("Hello world ^-^");
 
 	}
 
